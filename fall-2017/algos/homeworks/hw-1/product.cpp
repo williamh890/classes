c03ea3e5e9ee24ede2414c4d10b7a295085cd9cf
William Horn <wbhorn@alaska.edu>
2017-09-12 22:08:09 -0800

    Homework 1 finished for algos
    Started making make file to build it as practice.

--- fall-2017/algos/homeworks/hw-1/product.cpp
+++ fall-2017/algos/homeworks/hw-1/product.cpp
@@ -3,7 +3,7 @@
 // Sep 6, 2017
 // definitions of product class for hw-1 CS311
 
-#include "product.h"
+#include "product.h" // For Product
 
 #include<string>
 using std::string;
@@ -16,13 +16,20 @@ using std::ostringstream;
 #include<stdexcept>
 using std::runtime_error;
 
+// **********************************************************************
+// class Product - Member Definitions
+// **********************************************************************
 
+
+// Product::Product
+// (See header for docs.)
 Product::Product(string name, int sales) : _name(name) {
-    checkSales(sales);
-    _sales = sales;
+    setSales(sales);
 }
 
 
+// Product::checkSales
+// (See header for docs.)
 void Product::checkSales(int possible_sales) const {
     if (possible_sales < 0) {
         ostringstream out;
@@ -32,27 +39,37 @@ void Product::checkSales(int possible_sales) const {
 }
 
 
+// Product::getName
+// (See header for docs.)
 string Product::getName() const {
     return _name;
 }
 
 
+// Product::getSales
+// (See header for docs.)
 int Product::getSales() const {
     return _sales;
 }
 
 
+// Product::setName
+// (See header for docs.)
 void Product::setName(string newName){
     _name = newName;
 };
 
 
+// Product::setSales
+// (See header for docs.)
 void Product::setSales(int newSales) {
     checkSales(newSales);
     _sales = newSales;
 };
 
 
+// Product::toString
+// (See header for docs.)
 string Product::toString() const {
     ostringstream out;
     out << _name << " (sales: " << _sales << ")";
@@ -61,22 +78,30 @@ string Product::toString() const {
 };
 
 
+// Product::operator==
+// (See header for docs.)
 bool Product::operator==(const Product & other) const {
     return this->_name == other._name && this->_sales == other._sales;
 };
 
 
+// Product::operator!=
+// (See header for docs.)
 bool Product::operator!=(const Product & other) const {
     return !(*this == other);
 };
 
 
+// Product::operator++
+// (See header for docs.)
 Product& Product::operator++() {
     _sales++;
     return *this;
 }
 
 
+// Product::operator++
+// (See header for docs.)
 Product Product::operator++(int) {
     Product tmp(*this);
     operator++();
@@ -84,6 +109,8 @@ Product Product::operator++(int) {
 }
 
 
+// Product::operator--
+// (See header for docs.)
 Product& Product::operator--() {
     if (_sales <= 0) {
         _sales = 0;
@@ -91,17 +118,28 @@ Product& Product::operator--() {
     else {
         _sales--;
     }
+
     return *this;
 }
 
 
+// Product::operator--
+// (See header for docs.)
 Product Product::operator--(int) {
     Product tmp(*this);
     operator--();
+
     return tmp;
 }
 
+// **********************************************************************
+// class Product - Definitions of accociated global operators
+// **********************************************************************
 
+
+// operator<<
+// (See header for docs.)
 ostream& operator<<(ostream& out, const Product & p) {
    return out << p.toString();
 }
+
--- fall-2017/algos/homeworks/hw-1/product.h
+++ fall-2017/algos/homeworks/hw-1/product.h
@@ -10,38 +10,126 @@
 #include<string> // string
 #include<iostream> // ostream
 
+// **********************************************************************
+// class Product - Class Definition
+// **********************************************************************
 
+// class Product
+// Product: name, sales
+// Invariants:
+//      sales >= 0
 class Product {
     public:
+        // ********* Ctors, dctor, op= *********
+
+        // Ctor
+        // Sets name and sales uses default vales if name and sales
+        // not passed or if just name is passed
+        // Pre: more than just sales gets passed
+        // Post: name and sales will both be set
         Product(std::string name="", int sales=0);
 
+        // Big Five: using automatically written functions.
         ~Product() = default;
         Product(const Product & other) = default;
+        Product(Product && other) = default;
         Product & operator=(const Product & other) = default;
         Product & operator=(Product && other) = default;
 
-        std::string getName() const;
-        int getSales() const;
+        // ************ Operators *************
 
-        void setName(std::string newName);
-        void setSales(int newSales);
-
-        std::string toString() const;
-
+        // op==
+        // Pre: None
+        // Post:
+        //      only returns true if name and sales are both the same
+        //      between the two products
         bool operator==(const Product & other) const;
+
+        // op!=
+        // Pre: None
+        // Post:
+        //      only true if name or sales are not the same
+        //      between the two products
         bool operator!=(const Product & other) const;
 
+        // op++ [pre]
+        // Pre: None
+        // Post:
+        //      Sales is increamented by 1.
+        //      Return is *this.
         Product & operator++();
+
+        // op++ [post]
+        // Pre: None
+        // Post:
+        //      Sales is increamented by 1.
+        //      Return is the former value of *this.
         Product operator++(int dummy);
 
+        // op-- [pre]
+        // Pre: None
+        // Post:
+        //      Sales is decreased by 1, the value will not go below 0
+        //      Return is *this.
         Product & operator--();
+
+        // op-- [post]
+        // Pre: None
+        // Post:
+        //      Sales is decreased by 1, the value will not go below 0
+        //      Return is the former value of *this.
         Product operator--(int dummy);
 
+
+        // ********* Member Functions ***********
+
+        // getName
+        // Pre: None
+        // Post: Returns the _name property as a string
+        std::string getName() const;
+
+        // getSales
+        // Pre: None
+        // Post:
+        //      a non negative integer that is the number
+        //      of sales will be returned
+        int getSales() const;
+
+        // setName
+        // Pre: None
+        // Post:
+        //      the name will be set to the value of newName
+        void setName(std::string newName);
+
+        // setSales
+        // Pre: None
+        // Post:
+        //      sales will be set to the new value, if that value
+        //      is negative, an error will be thrown
+        void setSales(int newSales);
+
+        // toString
+        // Pre: None
+        // Post:
+        //      string containing class data will be return in the
+        //      form
+        //          <name> (sales: <sales>)
+        std::string toString() const;
+
     private:
+        // ********* Private Member Functions ************
+        //
+        // checkSales
+        // Make sure that a sales value is valid
+        // Pre: None
+        // Post:
+        //      will throw and error if sales is negative
         void checkSales(int sales) const;
 
-        std::string _name;
-        int _sales;
+        // *******  Data Members ************
+
+        std::string _name;  //Name of the products
+        int _sales;         // Number of sales (non-negative)
 };
 
 std::ostream& operator<<(std::ostream& out, const Product & p);
