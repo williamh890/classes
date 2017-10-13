c312df9bdc72fce810a29e683c65a6ebc0071acc
William Horn <wbhorn@alaska.edu>
2017-10-12 18:03:09 -0800

    Threads working!!

--- fall-2017/algos/homeworks/hw-4/counthsr.cpp
+++ fall-2017/algos/homeworks/hw-4/counthsr.cpp
@@ -5,6 +5,7 @@
 #include<iostream>
 #include<utility>
 using std::pair;
+using std::move;
 #include<vector>
 using std::vector;
 #include<numeric>
@@ -24,10 +25,15 @@ using Vec2I = pair<int, int>;
 using BoardType = vector<int>;
 using ChangePairType = pair<Vec2I, int>; // Position and value changed to
 
-const size_t THREAD_RUN_DIM = 20;
+const size_t THREAD_RUN_DIM = 15;
+
+
+class ThreadedHoleySpiderRun;
+
 
 class HoleySpiderRun {
     public:
+        friend class ThreadedHoleySpiderRun;
         // ***************** CONSTRUCTORS ******************
 
         // ctor
@@ -45,8 +51,7 @@ class HoleySpiderRun {
                     _start(start),
                     _finish(finish),
                     _initLeft((_dim.first * _dim.second) - 2),
-                    _initBoard(dim.first * dim.second, 1),
-                    _threadsAvailable(thread::hardware_concurrency()) {
+                    _initBoard(dim.first * dim.second, 1) {
 
             _get2D(_initBoard, _hole) = 0;
             _get2D(_initBoard, _start) = 0;
@@ -55,29 +60,13 @@ class HoleySpiderRun {
                     {-1,  1}, {0,  1}, {1,  1},
                     {-1,  0}  /*pos*/, {1,  0},
                     {-1, -1}, {0, -1}, {1, -1}};
-
-            std::cout << "Availble Threads: " << _threadsAvailable << std::endl;
         }
 
         // op()
         // call the object as a functition
         // pre: none
         int operator()() {
-            // only run threading on larger problems
-            if (_initLeft > THREAD_RUN_DIM && _threadsAvailable > 1) {
-                std::cout << "Availble Threads: " << _threadsAvailable << std::endl;
-                _thread_run(_initBoard, _start, _initLeft);
-
-                for (auto & t: _threads) {
-                    t.join();
-                }
-
-                return (int)accumulate(_returnVals.begin(), _returnVals.end(), 0);
-
-            }
-            else {
-                return _run(_initBoard, _start, _initLeft);
-            }
+            return _run(_initBoard, _start, _initLeft);
         }
 
     private:
@@ -123,55 +112,6 @@ class HoleySpiderRun {
             }
         }
 
-        void _thread_run(BoardType board, Vec2I pos, size_t squaresLeft) {
-            // Base case (probably wont happen, but just to be safe)
-            if (squaresLeft == 0 && pos == _finish) {
-                {
-                    lock_guard<mutex> lock(_threadLock);
-                    _returnVals.push_back(1);
-                }
-            }
-
-            // Recursive Case
-            else {
-                vector<Vec2I> toRecurse;
-                _validMoves(board, toRecurse, pos);
-
-                // Recuse on all possible moves
-                for(auto & pos : toRecurse) {
-                    bool startNewThread = false;
-
-                    // scope for lock_guard
-                    {
-                        lock_guard<mutex> lock(_threadLock);
-
-                        if (_threadsAvailable > 0) {
-                            _threadsAvailable -= 1;
-                            startNewThread = true;
-                        }
-                    }
-
-                    _get2D(board, pos) = 0;
-
-                    if (startNewThread) {
-                        _threads.push_back(
-                            thread(&HoleySpiderRun::_thread_run, this, board, pos, squaresLeft - 1 )
-                        );
-                    }
-
-                    else {
-
-                        auto to_add = _run(board, pos, squaresLeft - 1);
-                        {
-                            lock_guard<mutex> lock(_threadLock);
-                            _returnVals.push_back(to_add);
-                        }
-                    }
-                    _get2D(board, pos) = 1;
-                }
-            }
-        }
-
         // _run
         // recursive workhorse function for computations
         // pre: covered by ctor preconditions
@@ -210,11 +150,108 @@ class HoleySpiderRun {
         BoardType _initBoard;              // inital board state
         vector<Vec2I> _adjacentSquares;    // Relative coords of all neighboring squares
 
-        size_t _threadsAvailable;
-        vector<thread> _threads;
-        mutex _threadLock;
-        vector<int> _returnVals;
 };
+
+
+class ThreadedHoleySpiderRun {
+public:
+
+    ThreadedHoleySpiderRun(
+                const Vec2I & dim,
+                const Vec2I & hole,
+                const Vec2I & start,
+                const Vec2I & finish
+            ) : _run(HoleySpiderRun(dim, hole, start, finish)),
+                _threadsAvailable(thread::hardware_concurrency()),
+                _isThreadPass(true){
+        }
+
+        // op()
+        // call the object as a functition
+        // pre: none
+        int operator()() {
+            // only run threading on larger problems
+            if (_run._initLeft > THREAD_RUN_DIM && _run._dim.first > 3 && _run._dim.second > 3 && _threadsAvailable > 1) {
+                std::cout << "Availble Threads: " << _threadsAvailable << std::endl;
+
+                _thread_run(_run, _run._initBoard, _run._start, _run._initLeft);
+
+                return (int)accumulate(_returnVals.begin(), _returnVals.end(), 0);
+
+            }
+            else {
+                return _run();
+            }
+
+
+
+
+        }
+
+
+private:
+    void _thread_run(HoleySpiderRun run, BoardType board, Vec2I pos, size_t squaresLeft) {
+        // Base case (probably wont happen, but just to be safe)
+        if (squaresLeft == 0 && pos == run._finish) {
+            {
+                lock_guard<mutex> lock(_threadLock);
+                _returnVals.push_back(1);
+            }
+        }
+
+        // Recursive Case
+        else {
+            vector<Vec2I> toRecurse;
+            run._validMoves(board, toRecurse, pos);
+
+            // Recuse on all possible moves
+            for(auto & pos : toRecurse) {
+                // scope for lock_guard
+                {
+                    lock_guard<mutex> lock(_threadLock);
+
+                    if (_isThreadPass && _threadsAvailable > 1 ) {
+                        _threadsAvailable -= 1;
+
+                        _threads.push_back(
+                            move(thread(&ThreadedHoleySpiderRun::_thread_run_main, this, _run, pos, squaresLeft - 1 ))
+                        );
+                    }
+                }
+            }
+
+            for(auto & t : _threads) {
+                t.join();
+            }
+        }
+    }
+
+    void _thread_run_main(HoleySpiderRun run, Vec2I pos, size_t squaresLeft) {
+            run._get2D(run._initBoard, pos) = 0;
+            run._initLeft -= 1;
+            run._start = pos;
+            auto to_add = run();
+
+            {
+                std::cout << "to_add" << std::endl;
+                lock_guard<mutex> lock(_threadLock);
+                _returnVals.push_back(to_add);
+            }
+
+        }
+
+
+    HoleySpiderRun _run;
+    vector<HoleySpiderRun> _run_objects;
+
+    size_t _threadsAvailable;
+    vector<thread> _threads;
+    mutex _threadLock;
+    vector<int> _returnVals;
+    bool _isThreadPass;
+
+};
+
 
 
 // (see header for docs)
@@ -223,7 +260,7 @@ int countHSR(int dim_x, int dim_y,
              int start_x, int start_y,
              int finish_x, int finish_y) {
 
-    HoleySpiderRun run{
+    ThreadedHoleySpiderRun run{
         Vec2I{dim_x, dim_y},
         Vec2I{hole_x, hole_y},
         Vec2I{start_x, start_y},
